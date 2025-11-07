a64c2eb9d7f0c2976ce13d83780fb6fec534ddd6
gimeast <slasnrndu@gmail.com>
2025-11-07 23:13:09 +0900

    nextjs 공식문서 학습
    - chapter 14

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -4,6 +4,8 @@ import {z} from 'zod'
 import { revalidatePath } from 'next/cache';
 import {redirect} from "next/navigation";
 import postgres from 'postgres';
+import {signIn} from "@/auth";
+import {AuthError} from "next-auth";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -90,4 +92,23 @@ export const deleteInvoice = async (id:string) => {
     WHERE id = ${id}
     `;
   revalidatePath('/dashboard/invoices')
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return '로그인 실패';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
