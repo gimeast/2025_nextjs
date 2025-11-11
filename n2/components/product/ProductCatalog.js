5235dadb157d718b802f216e167fed40b6a52324
gimeast <slasnrndu@gmail.com>
2025-11-11 22:00:12 +0900

    nextjs 구멍가게코딩단 학습 n2
    - 이미지 출력

--- n2/components/product/ProductCatalog.js
+++ n2/components/product/ProductCatalog.js
@@ -1,7 +1,15 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function ProductCatalog({ products, total, current, size }) {
   if (!products || products.length === 0) {
     throw new Error("No Products in this page");
   }
+
+  console.log("products", products);
+
+  const hasPrev = current !== 1;
+  const hasNext = current !== Math.ceil(total / size);
 
   return (
     <div>
@@ -11,9 +19,22 @@ export default function ProductCatalog({ products, total, current, size }) {
             <div>PNO: {product.pno}</div>
             <div>NAME: {product.pname}</div>
             <div>PRICE: {product.price} </div>
+            <div className="relative w-1/3 h-40">
+              {/* 부모 크기 제한 */}
+              <Image
+                src={`http://localhost:8080/s_${product.fileName}`}
+                alt={product.pname}
+                fill
+                style={{ objectFit: "cover" }}
+                sizes="33vw"
+                priority={true}
+              />
+            </div>
           </li>
         ))}
       </ul>
+      {hasPrev && <Link href={`/product/catalog/${current - 1}`}>Prev</Link>}
+      {hasNext && <Link href={`/product/catalog/${current + 1}`}>Next</Link>}
     </div>
   );
 }
--- n2/next.config.mjs
+++ n2/next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http', // 이미지 프로토콜 (http, https)
+        hostname: 'localhost', // 이미지 호스트 이름 (IP 주소 또는 도메인)
+        port: '8080', // 포트 번호 (필요한 경우)
+        pathname: '/**', // 허용할 경로 패턴 (모든 경로 허용 시 '/**')
+      },
+    ],
+  },
 };
 
 export default nextConfig;
