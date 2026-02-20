6e69429e7f6e1ebe1a25c3bc0961951a3f87a44b
gimeast <slasnrndu@gmail.com>
2026-02-20 22:02:54 +0900

    한 입 크기로 잘라먹는 Nest.js Page Router
    - 한입북스 UI 구현

--- onebite-next/section02/src/pages/index.tsx
+++ onebite-next/section02/src/pages/index.tsx
@@ -1,9 +1,26 @@
 import style from './index.module.css';
 import { ReactNode } from 'react';
 import SearchableLayout from '@/components/searchable-layout';
+import BookItem from '@/components/book-item';
+import books from '@/mock/books.json';
 
 const Home = () => {
-  return <h1 className={style.h1}>Index</h1>;
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+    </div>
+  );
 };
 
 Home.getLayout = (page: ReactNode) => {
--- onebite-next/section02/src/pages/search/index.tsx
+++ onebite-next/section02/src/pages/search/index.tsx
@@ -1,11 +1,16 @@
-import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
 import SearchableLayout from '@/components/searchable-layout';
+import books from '@/mock/books.json';
+import BookItem from '@/components/book-item';
 
 const Page = () => {
-  const router = useRouter();
-  const { q } = router.query;
-  return <div>Search {q}</div>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 };
 
 Page.getLayout = (page: ReactNode) => {
