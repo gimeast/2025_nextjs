3feff2889da4253969349f2c1758e60b8d18b228
gimeast <slasnrndu@gmail.com>
2026-02-23 11:04:15 +0900

    한 입 크기로 잘라먹는 Nest.js Page Router
    - SEO

--- onebite-next/section02/src/pages/search/index.tsx
+++ onebite-next/section02/src/pages/search/index.tsx
@@ -4,6 +4,7 @@ import { ReactNode, useEffect, useState } from 'react';
 import fetchBooks from '@/lib/fetch-books';
 import { useRouter } from 'next/router';
 import { BookData } from '@/types';
+import Head from 'next/head';
 
 const Page = () => {
   const [books, setBooks] = useState<BookData[]>([]);
@@ -22,11 +23,22 @@ const Page = () => {
     }
   }, [q]);
   return (
-    <div>
-      {books.map((book) => (
-        <BookItem key={book.id} {...book} />
-      ))}
-    </div>
+    <>
+      <Head>
+        <title>한입북스 - 검색결과</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스" />
+        <meta
+          property="og:description"
+          content="한입 북스에 등록된 도서들을 만나보세요"
+        />
+      </Head>
+      <div>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </div>
+    </>
   );
 };
 
