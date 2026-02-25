c7c7b31df557cf63b9bc82004d164ec05e280a22
gimeast <slasnrndu@gmail.com>
2026-02-25 15:50:31 +0900

    한 입 크기로 잘라먹는 Nest.js App Router
    - 네비게이팅
    - 프리페칭

--- onebite-next/section03/src/app/(with-searchbar)/search/page.tsx
+++ onebite-next/section03/src/app/(with-searchbar)/search/page.tsx
@@ -1,10 +1,19 @@
+import ClientComponent from '@/components/client-component';
+
 const Page = async ({
   searchParams,
 }: {
   searchParams: Promise<{ q: string }>;
 }) => {
   const { q } = await searchParams;
-  return <div>검색 페이지 : {q}</div>;
+  return (
+    <div>
+      검색 페이지 : {q}
+      <ClientComponent>
+        <></>
+      </ClientComponent>
+    </div>
+  );
 };
 
 export default Page;
--- onebite-next/section03/src/app/book/[id]/page.tsx
+++ onebite-next/section03/src/app/book/[id]/page.tsx
@@ -1,6 +1,15 @@
+import ClientComponent from '@/components/client-component';
+
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
-  return <div>상세 페이지 : {id}</div>;
+  return (
+    <div>
+      상세 페이지 : {id}
+      <ClientComponent>
+        <></>
+      </ClientComponent>
+    </div>
+  );
 };
 
 export default Page;
