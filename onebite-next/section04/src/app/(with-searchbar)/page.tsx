dfca799664932b4fec5d8381e00b455707daab7a
gimeast <slasnrndu@gmail.com>
2026-03-01 22:20:12 +0900

    한 입 크기로 잘라먹는 Nest.js App Router
    - 라우트 세그먼트 옵션

--- onebite-next/section04/src/app/(with-searchbar)/page.tsx
+++ onebite-next/section04/src/app/(with-searchbar)/page.tsx
@@ -2,6 +2,9 @@ import BookItem from '@/components/book-item';
 import style from './page.module.css';
 import { BookData } from '@/types';
 
+//auto, force-dynamic, force-static, error
+// export const dynamic = 'force-dynamic';
+
 const AllBooks = async () => {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
