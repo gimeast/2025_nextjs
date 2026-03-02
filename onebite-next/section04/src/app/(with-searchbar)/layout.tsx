3a86271a2e9d94661f374cc4932be38a9be727d8
gimeast <slasnrndu@gmail.com>
2026-03-02 14:17:15 +0900

    한 입 크기로 잘라먹는 Nest.js App Router
    - 클라이언트 라우터 캐시

--- onebite-next/section04/src/app/(with-searchbar)/layout.tsx
+++ onebite-next/section04/src/app/(with-searchbar)/layout.tsx
@@ -4,6 +4,10 @@ import Searchbar from '../../components/searchbar';
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
+      <div>
+        클라이언트 라우터 캐시 확인을 위한 Date 출력:
+        {new Date().toLocaleString()}
+      </div>
       <Suspense fallback={<div>Loading...</div>}>
         <Searchbar />
       </Suspense>
