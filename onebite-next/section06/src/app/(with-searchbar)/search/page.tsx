7aa9343376bccd0a176e0e3a4ab3f8a1d338ad29
gimeast <slasnrndu@gmail.com>
2026-03-02 19:07:34 +0900

    한 입 크기로 잘라먹는 Nest.js App Router
    - 스트리밍 2

--- onebite-next/section06/src/app/(with-searchbar)/search/page.tsx
+++ onebite-next/section06/src/app/(with-searchbar)/search/page.tsx
@@ -1,14 +1,9 @@
 import BookItem from '@/components/book-item';
 import { BookData } from '@/types';
 import { delay } from '@/util/delay';
+import { Suspense } from 'react';
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{ q?: string }>;
-}) {
-  const { q } = await searchParams;
-
+async function SearchResult({ q }: { q: string }) {
   await delay(1500);
 
   const response = await fetch(
@@ -26,5 +21,19 @@ export default async function Page({
         <BookItem key={book.id} {...book} />
       ))}
     </div>
+  );
+}
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
+  const { q } = await searchParams;
+
+  return (
+    <Suspense key={q || ''} fallback={<div>Loading...</div>}>
+      <SearchResult q={q || ''} />
+    </Suspense>
   );
 }
