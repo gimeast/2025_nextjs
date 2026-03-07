da32ce5b36a0f03deda15af45abba50d3c0d3c4c
gimeast <slasnrndu@gmail.com>
2026-03-07 10:54:21 +0900

    한 입 크기로 잘라먹는 React.js 실전 프로젝트 SNS편
    - Shadcn/ui 사용2

--- onebite-react/onebite-exam/onebite-exam/src/App.tsx
+++ onebite-react/onebite-exam/onebite-exam/src/App.tsx
@@ -1,12 +1,32 @@
 import './App.css';
 import { Button } from '@/components/ui/button.tsx';
 import { cn } from '@/lib/utils.ts';
+import { Input } from '@/components/ui/input.tsx';
+import { Textarea } from '@/components/ui/textarea.tsx';
+import { Toaster } from '@/components/ui/sonner.tsx';
+import { toast } from 'sonner';
 
 function App() {
   const isActive = true;
   return (
-    <div>
-      <Button>버튼!</Button>
+    <div className="p-5">
+      <Toaster />
+      <Input placeholder="입력..." value="기본값" />
+      <Textarea />
+      <Button
+        onClick={() =>
+          toast('이벤트 발생!', {
+            position: 'top-center',
+          })
+        }
+      >
+        Sonner Event 버튼!
+      </Button>
+      <Button variant="destructive">버튼</Button>
+      <Button variant="ghost">버튼</Button>
+      <Button variant="link">버튼</Button>
+      <Button variant="outline">버튼</Button>
+      <Button variant="secondary">버튼</Button>
       <div
         className={cn(
           'w-10 text-sm',
