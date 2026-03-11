66b2c7f50948636f5caf712d3f9ed3153c59ff64
gimeast <slasnrndu@gmail.com>
2026-03-11 17:06:15 +0900

    한 입 크기로 잘라먹는 React.js 실전 프로젝트 SNS편
    - Zustand 기본 사용법 4
    - subscribeWithSelector, persist, devtools사용법 학습

--- onebite-react/onebite-exam/onebite-exam/src/store/count.ts
+++ onebite-react/onebite-exam/onebite-exam/src/store/count.ts
@@ -1,33 +1,64 @@
 import { create } from 'zustand';
-import { combine } from 'zustand/middleware';
+import {
+  combine,
+  subscribeWithSelector,
+  persist,
+  createJSONStorage,
+  devtools,
+} from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 
 //combine 미들웨어 사용시 state 타입이 자동으로 추론된다.
 export const useCountStore = create(
-  //immer는 복잡한 state를 다룰때 업데이트를 간편하게 해준다. 불변성을 유지하면서 간결하게 업데이트를 할수있도록 도와주는 미들웨어이다.
-  immer(
-    combine({ count: 0 }, (set, get) => ({
-      actions: {
-        increase: () => {
-          //명시되어있는 프로퍼티의 값만 업데이트 하기때문에 나머지(increase, decrease)는 명시하지 않아도 된다.
-          // set((state) => ({
-          //   count: state.count + 1,
-          // }));
-          set((state) => {
-            state.count += 1;
-          });
-        },
-        decrease: () => {
-          // set((state) => ({
-          //   count: state.count - 1,
-          // }));
-          set((state) => {
-            state.count -= 1;
-          });
-        },
+  //미들웨어는 순서가 중요하다 현재 순서를 기억해라
+  devtools(
+    persist(
+      subscribeWithSelector(
+        //immer는 복잡한 state를 다룰때 업데이트를 간편하게 해준다. 불변성을 유지하면서 간결하게 업데이트를 할수있도록 도와주는 미들웨어이다.
+        immer(
+          combine({ count: 0 }, (set, get) => ({
+            actions: {
+              increase: () => {
+                //명시되어있는 프로퍼티의 값만 업데이트 하기때문에 나머지(increase, decrease)는 명시하지 않아도 된다.
+                // set((state) => ({
+                //   count: state.count + 1,
+                // }));
+                set((state) => {
+                  state.count += 1;
+                });
+              },
+              decrease: () => {
+                // set((state) => ({
+                //   count: state.count - 1,
+                // }));
+                set((state) => {
+                  state.count -= 1;
+                });
+              },
+            },
+          })),
+        ),
+      ),
+      {
+        name: 'countStore',
+        partialize: (store) => ({ count: store.count }),
+        storage: createJSONStorage(() => sessionStorage),
       },
-    })),
+    ),
+    {
+      name: 'countStore',
+    },
   ),
+);
+
+useCountStore.subscribe(
+  (store) => store.count,
+  (state, prevState) => {
+    //선택된 값이 변경될때마다 호출되는 두번째 콜백함수를 Listener라고 한다
+    console.log(state, prevState);
+
+    const store = useCountStore.getState();
+  },
 );
 
 type Store = {
