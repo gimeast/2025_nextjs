929bf75572d8ca3c9a4dc94340b18a474225ae35
gimeast <slasnrndu@gmail.com>
2026-03-11 11:18:04 +0900

    한 입 크기로 잘라먹는 React.js 실전 프로젝트 SNS편
    - Zustand 기본 사용법 3
    - combine, immer 사용법 학습

--- onebite-react/onebite-exam/onebite-exam/src/store/count.ts
+++ onebite-react/onebite-exam/onebite-exam/src/store/count.ts
@@ -1,4 +1,34 @@
 import { create } from 'zustand';
+import { combine } from 'zustand/middleware';
+import { immer } from 'zustand/middleware/immer';
+
+//combine 미들웨어 사용시 state 타입이 자동으로 추론된다.
+export const useCountStore = create(
+  //immer는 복잡한 state를 다룰때 업데이트를 간편하게 해준다. 불변성을 유지하면서 간결하게 업데이트를 할수있도록 도와주는 미들웨어이다.
+  immer(
+    combine({ count: 0 }, (set, get) => ({
+      actions: {
+        increase: () => {
+          //명시되어있는 프로퍼티의 값만 업데이트 하기때문에 나머지(increase, decrease)는 명시하지 않아도 된다.
+          // set((state) => ({
+          //   count: state.count + 1,
+          // }));
+          set((state) => {
+            state.count += 1;
+          });
+        },
+        decrease: () => {
+          // set((state) => ({
+          //   count: state.count - 1,
+          // }));
+          set((state) => {
+            state.count -= 1;
+          });
+        },
+      },
+    })),
+  ),
+);
 
 type Store = {
   count: number;
@@ -7,8 +37,8 @@ type Store = {
     decrease: () => void;
   };
 };
-
 // store 생성
+/*
 export const useCountStore = create<Store>((set, get) => ({
   count: 0, //state
   actions: {
@@ -25,6 +55,7 @@ export const useCountStore = create<Store>((set, get) => ({
     }, //action
   },
 }));
+*/
 
 export const useCount = () => {
   const count = useCountStore((store) => store.count);
