caf42269066d6913427818ea12d9de7a12779623
gimeast <slasnrndu@gmail.com>
2026-03-13 16:13:06 +0900

    한 입 크기로 잘라먹는 React.js 실전 프로젝트 SNS편
    - tanstack query 사용법 6

--- onebite-react/onebite-exam/onebite-exam/src/hooks/mutations/use-create-todo-mutation.ts
+++ onebite-react/onebite-exam/onebite-exam/src/hooks/mutations/use-create-todo-mutation.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { createTodo } from '@/api/create-todo.ts';
 import { QUERY_KEYS } from '@/lib/constant.ts';
+import type { Todo } from '@/types.ts';
 
 export function useCreateTodoMutation() {
   const queryClient = useQueryClient();
@@ -9,9 +10,13 @@ export function useCreateTodoMutation() {
     mutationFn: createTodo,
     onMutate: () => {}, // 요청이 시작되었을때
     onSettled: () => {}, // 요청이 종료되었을때
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: QUERY_KEYS.todo.list,
+    onSuccess: (newTodo) => {
+      // 이렇게 invalidateQueries를 안사용하고 캐시 데이터를 직접 수정하여 refetch 없이 새로운 데이터를 화면에 바로 반영할수있다.
+      // invalidate 방식은 item의 갯수가 많아지면 리페칭해야하는 데이터양이 증가하여 서버부하를 일으킬수있으므로
+      // setQueryData를 사용하여 캐시에 직접 추가하여 서버부하를 방지할수있다.
+      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+        if (!prevTodos) return [newTodo];
+        return [...prevTodos, newTodo];
       });
     }, //요청이 성공하였을때
     onError: (error) => {
