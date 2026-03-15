34853edaf35133a9adaa97d8e8a7306104d797b5
gimeast <slasnrndu@gmail.com>
2026-03-15 10:31:05 +0900

    한 입 크기로 잘라먹는 React.js 실전 프로젝트 SNS편
    - tanstack query 사용법 8

--- onebite-react/onebite-exam/onebite-exam/src/hooks/mutations/use-update-todo-mutation.ts
+++ onebite-react/onebite-exam/onebite-exam/src/hooks/mutations/use-update-todo-mutation.ts
@@ -8,7 +8,14 @@ export function useUpdateTodoMutation() {
 
   return useMutation({
     mutationFn: updateTodo,
-    onMutate: (updatedTodo) => {
+    onMutate: async (updatedTodo) => {
+      //조회요청이 완료된 이후 캐시가 이전 데이터로 업데이트 되지않도록 데이터 조회요청을 취소시킨다.
+      await queryClient.cancelQueries({ queryKey: QUERY_KEYS.todo.list });
+
+      //낙관적 업데이트를 할경우 api 통신 실패시 이전 값으로 되돌려야하므로 이전 todolist를 캐시에서 가져온다
+      const prevTodos = queryClient.getQueryData<Todo[]>(QUERY_KEYS.todo.list);
+
+      //낙관적 업데이트
       queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
         if (!prevTodos) return [];
         return prevTodos.map((prevTodo) =>
@@ -16,6 +23,24 @@ export function useUpdateTodoMutation() {
             ? { ...prevTodo, ...updatedTodo }
             : prevTodo,
         );
+      });
+
+      return {
+        prevTodos,
+      };
+    },
+    onError: (error, variables2, context) => {
+      //context는 onMutate에서 반환한 값이된다.
+      if (context && context.prevTodos) {
+        queryClient.setQueryData<Todo[]>(
+          QUERY_KEYS.todo.list,
+          context.prevTodos,
+        );
+      }
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEYS.todo.list,
       });
     },
   });
