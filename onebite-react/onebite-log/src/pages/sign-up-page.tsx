1c4ce2163e04d010ea4f70b40fc075d306c4ce54
gimeast <slasnrndu@gmail.com>
2026-03-23 11:17:53 +0900

    한 입 크기로 잘라먹는 React.js 실전 프로젝트 SNS편
    - 회원가입 구현

--- onebite-react/onebite-log/src/pages/sign-up-page.tsx
+++ onebite-react/onebite-log/src/pages/sign-up-page.tsx
@@ -1,5 +1,51 @@
+import { Input } from "@/components/ui/input.tsx";
+import { Button } from "@/components/ui/button.tsx";
+import { Link } from "react-router";
+import { useState } from "react";
+import { useSignUp } from "@/hools/mutations/use-sign-up.ts";
+
 const SignUpPage = () => {
-  return <div></div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { mutate: signUp } = useSignUp();
+
+  const handleSignUpClick = () => {
+    if (email.trim() === "" || password.trim() === "") return;
+    signUp({ email, password });
+  };
+
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="text-xl font-bold">회원가입</div>
+      <div className="flex flex-col gap-2">
+        <Input
+          className="py-6"
+          type="email"
+          placeholder="example@abc.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          className="py-6"
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <Button className="w-full" onClick={handleSignUpClick}>
+          회원가입
+        </Button>
+      </div>
+      <div>
+        <Link className="text-muted-foreground hover:underline" to="/sign-in">
+          이미 계정이 있다면 로그인
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;
