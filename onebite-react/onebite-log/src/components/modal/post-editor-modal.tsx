9b00390b3735ac4a823079cb9d4dd3f5bde1c550
gimeast <slasnrndu@gmail.com>
2026-03-25 14:00:00 +0900

    한 입 크기로 잘라먹는 React.js 실전 프로젝트 SNS편
    - Post 추가 기능 구현

--- onebite-react/onebite-log/src/components/modal/post-editor-modal.tsx
+++ onebite-react/onebite-log/src/components/modal/post-editor-modal.tsx
@@ -3,6 +3,8 @@ import { Button } from "@/components/ui/button.tsx";
 import { ImageIcon } from "lucide-react";
 import { usePostEditorModal } from "@/store/post-editor-modal.ts";
 import { useEffect, useRef, useState } from "react";
+import { useCreatePost } from "@/hooks/mutations/post/use-create-post.ts";
+import { toast } from "sonner";
 
 const PostEditorModal = () => {
   const { isOpen, close } = usePostEditorModal();
@@ -10,8 +12,23 @@ const PostEditorModal = () => {
   const [content, setContent] = useState("");
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
+  const { mutate: createPost, isPending: isCreatePostPending } = useCreatePost({
+    onSuccess: () => {
+      toast.error("저장 완료", { position: "top-center" });
+      close();
+    },
+    onError: (error) => {
+      toast.error("게시물 생성을 실패했습니다.", { position: "top-center" });
+    },
+  });
+
   const handleCloseModal = () => {
     close();
+  };
+
+  const handleCreatePostClick = () => {
+    if (content.trim() === "") return;
+    createPost(content);
   };
 
   useEffect(() => {
@@ -38,12 +55,23 @@ const PostEditorModal = () => {
           placeholder="무슨 일이 있었나요?"
           value={content}
           onChange={(e) => setContent(e.target.value)}
+          disabled={isCreatePostPending}
         />
-        <Button variant="outline" className="cursor-pointer">
+        <Button
+          variant="outline"
+          className="cursor-pointer"
+          disabled={isCreatePostPending}
+        >
           <ImageIcon />
           이미지 추가
         </Button>
-        <Button className="cursor-pointer">저장</Button>
+        <Button
+          className="cursor-pointer"
+          onClick={handleCreatePostClick}
+          disabled={isCreatePostPending}
+        >
+          저장
+        </Button>
       </DialogContent>
     </Dialog>
   );
--- onebite-react/onebite-log/src/pages/reset-password-page.tsx
+++ onebite-react/onebite-log/src/pages/reset-password-page.tsx
@@ -1,7 +1,7 @@
 import { Input } from "@/components/ui/input.tsx";
 import { Button } from "@/components/ui/button.tsx";
 import { useState } from "react";
-import { useUpdatePassword } from "@/hooks/mutations/use-reset-password.ts";
+import { useUpdatePassword } from "@/hooks/mutations/auth/use-reset-password.ts";
 import { toast } from "sonner";
 import { generateErrorMessage } from "@/lib/error.ts";
 import { useNavigate } from "react-router";
